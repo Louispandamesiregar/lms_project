3355458466493a2d72241f69b9de5b49caeee35c
Louispandamesiregar <louispandame@gmail.com>
2026-08-27 17:50:49 +0700

    style: shift image row to the right to fill empty space

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -42,8 +42,8 @@ export default async function CoursesPage({ searchParams }: { searchParams: Prom
             </p>
           </div>
 
-          {/* Decorative Retro Stickers - Aligned in a row, overlapping and larger */}
-          <div className="hidden lg:flex items-center -space-x-16 xl:-space-x-20 z-20 pointer-events-none">
+          {/* Decorative Retro Stickers - Aligned in a row, overlapping and shifted right */}
+          <div className="hidden lg:flex items-center -space-x-16 xl:-space-x-20 z-20 pointer-events-none translate-x-12 lg:translate-x-24 xl:translate-x-32">
             <img src="/images/desain grafis.png" alt="" className="w-48 h-48 xl:w-60 xl:h-60 object-contain drop-shadow-[0_0_5px_rgba(255,255,255,0.8)] drop-shadow-[4px_4px_0px_rgba(26,26,26,1)] relative z-10" />
             <img src="/images/fotografi.png" alt="" className="w-48 h-48 xl:w-60 xl:h-60 object-contain drop-shadow-[0_0_5px_rgba(255,255,255,0.8)] drop-shadow-[4px_4px_0px_rgba(26,26,26,1)] relative z-20" />
             <img src="/images/seni lukis.png" alt="" className="w-48 h-48 xl:w-60 xl:h-60 object-contain drop-shadow-[0_0_5px_rgba(255,255,255,0.8)] drop-shadow-[4px_4px_0px_rgba(26,26,26,1)] relative z-30" />
